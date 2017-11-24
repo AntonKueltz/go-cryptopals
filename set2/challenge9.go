98dccd43d18085c1bf528a79362709dded212cd9
AntonKueltz <kueltz.anton@gmail.com>
2017-11-24 00:19:44 -0800

    add set2 folder and first challenge

---
+++ set2/challenge9.go
@@ -0,0 +1,28 @@
+package set2
+
+import "fmt"
+
+// PadPkcs7 pads a byte array accoring to the PKCS#7 standard
+func PadPkcs7(bytes []byte, blocksize int) []byte {
+    byteLen := len(bytes)
+    paddingBytes := blocksize - (byteLen % blocksize)
+    if paddingBytes == 0 {
+        paddingBytes = blocksize
+    }
+
+    paddedBytes := make([]byte, byteLen + paddingBytes)
+    copy(paddedBytes[:byteLen], bytes[:])
+
+    for i := byteLen; i < byteLen + paddingBytes; i++ {
+        paddedBytes[i] = byte(paddingBytes)
+    }
+
+    return paddedBytes
+}
+
+// Challenge9Main solves set 2 challenge 9
+func Challenge9Main() {
+    stringToPad := []byte("YELLOW SUBMARINE")
+    padded := PadPkcs7(stringToPad, 20)
+    fmt.Println(padded)
+}
